ba87c36b490c90ee3ec8dcd37a2af2efebd7a232
Bohumír Sláma <bohumir.slama>
2022-05-01 18:56:36 +0200

    Prvni verze

---
+++ IRawRequestStore.cs
@@ -0,0 +1,16 @@
+﻿namespace TechniServIT.ISRT.IK.Common
+{
+    /// <summary>
+    /// Slouží pro uložení a načtení surového těla requestu.
+    /// Voláno v <see cref="RawRequestResponseMiddleware"/>
+    /// Ukládá i KomunikaceId.
+    /// </summary>
+    public interface IRawRequestStore
+    {
+        void SaveRequestRawBody(string body);
+        string GetRequestRawBody();
+
+        void SaveKomunikaceId(long komunikaceId);
+        long GetKomunikaceId();
+    }
+}
