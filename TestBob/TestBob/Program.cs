583763321f13bf5dadf7b05963331a1679b7f4ef
Bohumír Sláma <bohumirslama81>
2022-05-01 19:33:24 +0200

    Treti verze main

--- TestBob/TestBob/Program.cs
+++ TestBob/TestBob/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("Hello, World 2!");
 Console.WriteLine("Hello, World 3!");
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 4!");
+Console.WriteLine("Hello, World 5!");
