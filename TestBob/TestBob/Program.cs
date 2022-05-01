712e70ee7dbff72ee7095e7e8e6d3d6c2c95a10b
Bohumír Sláma <bohumir.slama>
2022-05-01 19:02:18 +0200

    Treti verze

--- TestBob/TestBob/Program.cs
+++ TestBob/TestBob/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 2!");
+Console.WriteLine("Hello, World 1!");
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World 3!");
